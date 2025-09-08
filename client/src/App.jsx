00e8e4844ac88a0bed64da8d64b751c3d222cb44
Chanuka999 <chanukaranditha99@gmail.com>
2025-09-09 01:08:38 +0530

    Refactor routing in App component to include Login and Register routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,14 +10,20 @@ import Feature from "./components/Feature";
 import About from "./components/About";
 import "./App.css";
 import React from "react";
+import Register from "./components/Register";
+import Login from "./components/Login";
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<RootLayout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="feature" element={<Feature />} />
-      </Route>
+      <>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="feature" element={<Feature />} />
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </>
     )
   );
   return <RouterProvider router={router} />;
