434619623f8e7d973da1bd05dad89f8fb249a1a7
Chanuka999 <chanukaranditha99@gmail.com>
2025-09-10 19:39:55 +0530

    Add health benefits section to Home component with relevant images and information

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import patientImage from "../assets/patient.jpg";
+import patientImage1 from "../assets/patient1.jpg";
 
 const Home = () => {
   return (
@@ -21,6 +22,34 @@ const Home = () => {
             alt="patient"
             className="w-full h-auto rounded-lg"
           />
+        </div>
+      </div>
+      <div className="flex flex-col md:flex-row items-center justify-between">
+        <div className="md:w-1/2 p-4">
+          <img
+            src={patientImage1}
+            alt="patient"
+            className="w-full h-auto rounded-lg"
+          />
+        </div>
+        <div className="md:w-1/2 p-4">
+          <h1 className="text-3xl font-bold mb-4">
+            Health and Economic Benefits of Chronic Disease Interventions
+          </h1>
+          <p className="text-lg text-gray-700">
+            <ul>
+              <li>
+                Chronic diseases are the leading causes of illness, disability,
+                and death in the world. They are also the leading drivers of our
+                nation's $4.9 trillion in annual health care costs.
+              </li>
+              <li>
+                {" "}
+                CDC funds partners to use proven interventions that improve
+                quality of life and save money.
+              </li>
+            </ul>
+          </p>
         </div>
       </div>
     </div>
