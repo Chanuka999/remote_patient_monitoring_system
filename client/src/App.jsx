46c1094524305e559f325056e158b1589552f9c9
Chanuka999 <chanukaranditha99@gmail.com>
2025-10-12 00:48:24 +0530

    Add symptom management routes and implement chat functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,14 @@ import PatientDashboardForm from "./components/PatientDashboardForm";
 import PatientDashboard from "./components/PatientDashboard";
 import DoctorDashboard from "./components/DoctorDashboard";
 import Appointment from "./components/Appointment";
+import Sympthoms from "./components/Sympthoms";
+import SymptomDetail from "./components/SymptomDetail";
+import SymptomAction from "./components/SymptomAction";
+import Diabetics from "./components/Sympthoms/DiabeticsForm";
+import Asthma from "./components/Sympthoms/Asethma";
+import HypertensionForm from "./components/Sympthoms/HypertentionForm";
+import Chat from "./Chat";
+
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
@@ -34,6 +42,13 @@ function App() {
           <Route path="patientDashboard" element={<PatientDashboard />} />
           <Route path="doctorDashboard" element={<DoctorDashboard />} />
           <Route path="appointment" element={<Appointment />} />
+          <Route path="Sympthoms" element={<Sympthoms />} />
+          <Route path="Sympthoms/:id" element={<SymptomDetail />} />
+          <Route path="Sympthoms/action/:id" element={<SymptomAction />} />
+          <Route path="Diabetics" element={<Diabetics />} />
+          <Route path="asthma" element={<Asthma />} />
+          <Route path="hypertension" element={<HypertensionForm />} />
+          <Route path="chat" element={<Chat />} />
         </Route>
       </>
     )
---
+++ client/src/Chat.jsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useRef, useState } from "react";
+import { IoChatbox } from "react-icons/io5";
+import { FaArrowDown } from "react-icons/fa";
+import ChatbotIcon from "./chatbot/ChatbotIcon";
+import ChatForm from "./chatbot/ChatForm";
+import ChatMessage from "./chatbot/ChatMessage";
+
+const Chat = () => {
+  const [chatHistory, setChatHistory] = useState([]);
+  const [showChatbot, setShowChatbot] = useState(false);
+  const chatBodyRef = useRef();
+
+  const generateBotResponce = async (history) => {
+    const updateHistory = (text) => {
+      setChatHistory((prev) => [
+        ...prev.filter((msg) => msg.text !== "Thinking..."),
+        { role: "model", text },
+      ]);
+    };
+    history = history.map(({ role, text }) => ({ role, parts: [{ text }] }));
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ contents: history }),
+    };
+    try {
+      const responce = await fetch(
+        import.meta.env.VITE_API_URL,
+        requestOptions
+      );
+      const data = await responce.json();
+      if (!responce.ok)
+        throw new Error(data.error.message || "something went wrong!");
+      const apiresponceText = data.candidates[0].content.parts[0].text
+        .replace(/\*\*(.*?)\*\*/g, "$1")
+        .trim();
+      updateHistory(apiresponceText);
+    } catch (error) {
+      updateHistory(error.message, true);
+    }
+  };
+
+  useEffect(() => {
+    chatBodyRef.current.scrollTo({
+      top: chatBodyRef.current.scrollHeight,
+      behavior: "smooth",
+    });
+  }, [chatHistory]);
+  return (
+    <div className={`chatbot-container ${showChatbot ? "show-chatbot" : ""}`}>
+      <button
+        onClick={() => setShowChatbot((prev) => !prev)}
+        id="chatbot-toggler"
+      >
+        <span className="material-symbols-outlined">
+          <IoChatbox size={25} />
+        </span>
+        {/* <span className="material-symbols-outlined">close</span> */}
+      </button>
+
+      <div className="chatbot-popup">
+        <div className="chat-header">
+          <div className="header-info">
+            <ChatbotIcon />
+            <h2 className="logo-text">Chatbot</h2>
+          </div>
+          <button
+            onClick={() => setShowChatbot((prev) => !prev)}
+            className="material-symbols-outlined"
+          >
+            <FaArrowDown />
+          </button>
+        </div>
+        <div ref={chatBodyRef} className="chat-body">
+          <div className="message bot-message">
+            <ChatbotIcon />
+            <p className="message-text">
+              Hey there <br /> How can I help you today?
+            </p>
+          </div>
+
+          {chatHistory.map((chat, index) => (
+            <ChatMessage key={index} chat={chat} />
+          ))}
+        </div>
+
+        <div className="chat-footer">
+          <ChatForm
+            chatHistory={chatHistory}
+            setChatHistory={setChatHistory}
+            generateBotResponce={generateBotResponce}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
