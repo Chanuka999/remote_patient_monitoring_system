79c490d2fb58e9353577f817d2a2c460f299a683
Chanuka999 <chanukaranditha99@gmail.com>
2026-01-02 20:51:54 +0530

    feat: Add endpoint for predicting based on symptoms

--- server/routes/predictRouter.js
+++ server/routes/predictRouter.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   predict,
   predictHeartFromForm,
+  predictFromSymptoms,
   chat,
 } from "../controllers/predictController.js";
 
@@ -9,6 +10,7 @@ const router = express.Router();
 
 router.post("/predict", predict);
 router.post("/predict/heart_from_form", predictHeartFromForm);
+router.post("/predict/symptoms", predictFromSymptoms);
 router.post("/chat", chat);
 
 export default router;
