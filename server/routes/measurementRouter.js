3753a2d5086d4b7af64c9e6e2f49c9d98ff2df10
Chanuka999 <chanukaranditha99@gmail.com>
2026-03-10 22:23:42 +0530

    add

--- server/routes/measurementRouter.js
+++ server/routes/measurementRouter.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { saveMeasurement } from "../controllers/measurementController.js";
+import {
+  getHealthTrends,
+  saveMeasurement,
+} from "../controllers/measurementController.js";
 
 const router = express.Router();
 
 router.post("/measurements", saveMeasurement);
+router.get("/measurements/trends", getHealthTrends);
 
 export default router;
